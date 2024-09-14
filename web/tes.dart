7ea7256f330714ae872edcd9889ba906ba3fd8cd
GibranAja <mgibranarraffi@gmail.com>
2024-09-14 18:06:35 +0700

    feat: add tes file for learn

--- web/tes.dart
+++ web/tes.dart
@@ -1,28 +1,40 @@
 void main() {
-  List jurusan = [
-    'Pak Ajid',
-    'Wanda Kurniawan',
-    'Bu Cynthia',
-    'Pa Bachtiar',
-    'Pa Salikin'
-  ];
-  List noTelp = [
-    0813400440,
-    082713567236,
-    083377236463,
-    08393926263,
-    086228728263
-  ];
+  // List jurusan = [
+  //   'Pak Ajid',
+  //   'Wanda Kurniawan',
+  //   'Bu Cynthia',
+  //   'Pa Bachtiar',
+  //   'Pa Salikin'
+  // ];
+  // List noTelp = [
+  //   0813400440,
+  //   082713567236,
+  //   083377236463,
+  //   08393926263,
+  //   086228728263
+  // ];
 
-  // Mapping
-  Map<String, Map<String, dynamic>> kaprog = {
-    'Animasi': {'Kaprog' : jurusan[0], 'No telp' : noTelp[0]},
-    'PPLG': {'Kaprog' : jurusan[1], 'No telp' : noTelp[1]},
-    'BCF': {'Kaprog' : jurusan[2], 'No telp' : noTelp[2]},
-    'TO': {'Kaprog' : jurusan[3], 'No telp' : noTelp[3]},
-    'TPFL': {'Kaprog' : jurusan[4], 'No telp' : noTelp[4]},
-  };
+  // // Mapping
+  // Map<String, Map<String, dynamic>> kaprog = {
+  //   'Animasi': {'Kaprog' : jurusan[0], 'No telp' : noTelp[0]},
+  //   'PPLG': {'Kaprog' : jurusan[1], 'No telp' : noTelp[1]},
+  //   'BCF': {'Kaprog' : jurusan[2], 'No telp' : noTelp[2]},
+  //   'TO': {'Kaprog' : jurusan[3], 'No telp' : noTelp[3]},
+  //   'TPFL': {'Kaprog' : jurusan[4], 'No telp' : noTelp[4]},
+  // };
 
-  print(jurusan);
-  print(kaprog);
+  // print(jurusan);
+  // print(kaprog);
+
+  List warna = ['Merah', 'Putih', 'Biru'];
+  print(warna);
+
+  warna.add('Merah');
+  print(warna);
+
+  Set<String> color = {'Merah', 'Putih', 'Biru'};
+  print(color);
+
+  color.add('Merah');
+  print(color);
 }
