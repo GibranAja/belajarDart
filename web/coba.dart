cc805a63cd7169f861cb9dcc8f9b4d8f9f5c5f49
GibranAja <mgibranarraffi@gmail.com>
2024-09-14 18:01:03 +0700

    feat: add void peminjam

---
+++ web/coba.dart
@@ -0,0 +1,11 @@
+void pinjamHp(String nama, String alamat, int hari) {
+  print('$nama kamu meminjam hp selama $hari hari, rumah kamu di $alamat yaaaa');
+}
+
+void peminjamHp() {
+  pinjamHp('Iban', 'Ciomas', 3);
+}
+
+void main() {
+  peminjamHp();
+}
