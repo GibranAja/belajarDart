04a5d59fd775dd12b6b482465aad24160933bc70
GibranAja <mgibranarraffi@gmail.com>
2024-09-23 11:48:22 +0700

    feat: add new cosntructor

--- web/oop.dart
+++ web/oop.dart
@@ -5,22 +5,41 @@ class Hewan{
   String? namaPemilik;
 
   // Constructor
-  // Hewan(this.kategori, this.nama, this.berat, this.namaPemilik);
-
   Hewan({this.kategori, this.nama, this.berat, this.namaPemilik});
+
+  // Method / Function
+  makan(int beratMakanan) {
+      berat = berat! + beratMakanan;
+    }
+
+    diet(int tidakMakan) {
+    berat = berat! - tidakMakan;
+  }
+
 }
-
-
 
 void main() {
 
-
-
-  var hewan1 = Hewan(kategori :'Reptil', nama: 'Ular', berat: 15, namaPemilik: "Cahyo");
+  var hewan1 = Hewan(kategori :'Reptil', nama: 'Ular', berat: 15, namaPemilik: "Eman");
   print(hewan1.nama);
   print('Nama Hewan ini adalah ${hewan1.nama}, dia berkategori sebagai ${hewan1.kategori}, berat dia sekitar ${hewan1.berat} KG');
 
+  hewan1.makan(10);
+
+  print(hewan1.nama);
+  print('Nama Hewan ini adalah ${hewan1.nama}, dia berkategori sebagai ${hewan1.kategori}, berat dia sekitar ${hewan1.berat} KG');
+
+  hewan1.diet(5);
+
+  print(hewan1.nama);
+  print('Nama Hewan ini adalah ${hewan1.nama}, dia berkategori sebagai ${hewan1.kategori}, berat dia sekitar ${hewan1.berat} KG');
+  
   print('-------------------------');
+  // Makan Iblis
+
+  hewan1.nama = "Dino";
+  print(hewan1.nama);
+  print('Nama Hewan ini adalah ${hewan1.nama}, dia berkategori sebagai ${hewan1.kategori}, berat dia sekitar ${hewan1.berat} KG');
 
   // var hewan2 = Hewan('Kucing', 'Mamalia', 5, 'Juned');
   // print(hewan2.nama);
